fe92b6b7174b92a579481c93f6454a0a5664d084
Wojciech Chojnowski <62705977+Wojtek27@users.noreply.github.com>
2021-11-03 11:58:24 +0100

    Delete program.cmd

--- etap1/program.cmd
+++
@@ -1,9 +0,0 @@
-#define LOW_MOVE_SPEED 10
-#define MEDIUM_MOVE_SPEED 20
-#define HIGH_MOVE_SPEED 30
-
-Move Alpha LOW_MOVE_SPEED 20   // wolny ruch do przodu
-Rotate Alpha 10 Z 180          // obrot o 180 stopni
-Pause Alpha 2000               // dwie sekundy przerwy
-Move Alpha HIGH_MOVE_SPEED 100 //szybki ruch do przodu
-End
